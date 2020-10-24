f2af18f61a2a2ac1aee5e2e88eb4b7dfdac28274
michelthomas <michel.lins17@gmail.com>
2020-10-24 00:33:46 -0300

    Criação de migrations para Orcamento, Categoria e Item;

---
+++ database/migrations/2020_10_24_014846_create_orcamentos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrcamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orcamentos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('nome');
+            $table->text('descricao');
+            $table->double('valor_estimado');
+            $table->double('valor_realizado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orcamentos');
+    }
+}
---
+++ database/migrations/2020_10_24_022721_create_categorias_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('orcamento_id');
+            $table->string('nome');
+            $table->text('descricao');
+            $table->double('valor_estimado');
+            $table->double('valor_realizado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+    }
+}
